bdab53edd88b56d924121b64e848d61039688e47
LEESR <sy870203>
2020-08-21 22:28:19 +0900

    user register #1

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const router = express.Router();
 
-
+const userModel = require('../models/user');
 
 
 // @route POST http://localhost:5000/user/register
@@ -9,6 +9,25 @@ const router = express.Router();
 // @access PUBLIC
 router.post('/register', (req, res) => {
 
+    const { name, email, password } = req.body;
+
+    const user = new userModel({
+        name, email, password
+    });
+
+    user
+        .save()
+        .then(user => {
+            res.status(200).json({
+                msg: "saved User",
+                userInfo: user
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                msg: err.message
+            })
+        });
 })
 
 // @route POST http://localhost:5000/user/login
@@ -22,7 +41,7 @@ router.post('/login', (req, res) => {
 // @desc Return current user
 // @access Private
 router.get('/current', (req, res) => {
-    
+
 })
 
 module.exports = router;
