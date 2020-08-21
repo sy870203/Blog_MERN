699ecdefedf543b93d8081f4c70b27c5ecb78d16
LEESR <sy870203>
2020-08-21 21:35:21 +0900

    add dotenv

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const logger = require('morgan');
 
 const mongoose = require('mongoose');
 
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 const app = express();
@@ -35,6 +37,6 @@ app.use(logger('dev'));
 
 
 
-const port = 5000;
+const PORT = process.env.PORT || 7000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
