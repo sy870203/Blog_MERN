5e740da56fcbca0ad1592d7a0679e7ec45344528
LEESR <sy870203>
2020-08-21 22:35:50 +0900

    add  bcrypt (password 암호화)

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 const userModel = require('../models/user');
 
@@ -11,23 +12,35 @@ router.post('/register', (req, res) => {
 
     const { name, email, password } = req.body;
 
-    const user = new userModel({
-        name, email, password
-    });
-
-    user
-        .save()
-        .then(user => {
-            res.status(200).json({
-                msg: "saved User",
-                userInfo: user
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
+    //패스워드 암호화
+    bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+            return res.status(400).json({
                 msg: err.message
-            })
-        });
+            });
+        } else {
+            const user = new userModel({
+                name, email, 
+                password: hash
+            });
+        
+            user
+                .save()
+                .then(user => {
+                    res.status(200).json({
+                        msg: "saved User",
+                        userInfo: user
+                    });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        msg: err.message
+                    })
+                });
+        }
+    })
+
+    
 })
 
 // @route POST http://localhost:5000/user/login
