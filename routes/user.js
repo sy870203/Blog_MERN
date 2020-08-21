f78c49d33fa3d96b67380193080912a1d23f197e
LEESR <sy870203>
2020-08-21 22:52:25 +0900

    회원가입 마지막 4번째

--- routes/user.js
+++ routes/user.js
@@ -11,42 +11,61 @@ const userModel = require('../models/user');
 // @access PUBLIC
 router.post('/register', (req, res) => {
 
+    // email 유무체크 => 패스워드 암호화 => 아바타 생성 => 회원가입
+
     const { name, email, password } = req.body;
 
-    //패스워드 암호화
-    bcrypt.hash(password, 10, (err, hash) => {
-        if (err) {
-            return res.status(400).json({
-                msg: err.message
-            });
-        } else {
-
-            const avatar = gravatar.url(email, {
-                s: '200', // size
-                r: 'pg',  // png
-                d: 'mm'   // Default
-            })
-
-            const user = new userModel({
-                name, email, avatar, 
-                password: hash
-            });
-        
-            user
-                .save()
-                .then(user => {
-                    res.status(200).json({
-                        msg: "saved User",
-                        userInfo: user
-                    });
+    userModel
+        .findOne({ email })
+        .then(user => {
+            if (user) {
+                return res.status(494).json({
+                    msg: "Email already exists"
                 })
-                .catch(err => {
-                    res.status(500).json({
-                        msg: err.message
-                    })
-                });
-        }
-    })
+            } else {
+                bcrypt.hash(password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(400).json({
+                            msg: err.message
+                        });
+                    } else {
+                        //패스워드 암호화
+                        const avatar = gravatar.url(email, {
+                            s: '200', // size
+                            r: 'pg',  // png
+                            d: 'mm'   // Default
+                        })
+            
+                        const user = new userModel({
+                            name, email, avatar, 
+                            password: hash
+                        });
+                    
+                        user
+                            .save()
+                            .then(user => {
+                                res.status(200).json({
+                                    msg: "saved User",
+                                    userInfo: user
+                                });
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    msg: err.message
+                                })
+                            });
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                msg: err.message
+            })
+        })
+
+    
+    
 
     
 })
